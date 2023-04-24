b96c1a12006b0f97dbdc10a7c89440c922358f2a
Eddy Rivera Echegaray <eddyriveraechegaray@Eddys-MBP.homenet.telecomitalia.it>
2023-04-24 12:08:24 +0200

    update server url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,6 @@ import Customizer from "./pages/Customizer";
 import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <main className="app transition-all ease-in">
       <Home />
